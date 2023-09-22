f4d79d9658b3a87bc56b1faa5d8452f1ad6f4ad1
Eugene S <eugene.spnr@mail.ru>
2023-09-22 18:07:14 +0300

    memstore tests

--- internal/storage/memstore/storege_test.go
+++ internal/storage/memstore/storege_test.go
@@ -4,10 +4,89 @@ package memstore
 
 import (
 	"context"
+	"path/filepath"
 	"testing"
 
 	"github.com/eugene982/url-shortener/internal/model"
+	"github.com/eugene982/url-shortener/internal/storage"
+	"github.com/stretchr/testify/require"
 )
+
+func TestNew(t *testing.T) {
+	testdata, err := filepath.Abs("testdata")
+	require.NoError(t, err)
+
+	_, err = New(testdata + "/err-url-db.json")
+	require.Error(t, err)
+
+	store, err := New(testdata + "/short-url-db.json")
+	require.NoError(t, err)
+
+	err = store.Close()
+	require.NoError(t, err)
+}
+
+func TestPing(t *testing.T) {
+	store, err := New("")
+	require.NoError(t, err)
+
+	ctx, close := context.WithCancel(context.Background())
+	err = store.Ping(ctx)
+	require.NoError(t, err)
+
+	close()
+	err = store.Ping(ctx)
+	require.Error(t, err)
+}
+
+func TestGetAddr(t *testing.T) {
+	store, err := New("")
+	require.NoError(t, err)
+
+	short := "short"
+	want := "ya.ru"
+
+	ctx, close := context.WithCancel(context.Background())
+	err = store.Set(ctx, model.StoreData{
+		OriginalURL: want,
+		ShortURL:    short})
+	require.NoError(t, err)
+
+	get, err := store.GetAddr(ctx, short)
+	require.NoError(t, err)
+	require.Equal(t, want, get.OriginalURL)
+
+	_, err = store.GetAddr(ctx, "-")
+	require.ErrorIs(t, err, storage.ErrAddressNotFound)
+
+	close()
+	_, err = store.GetAddr(ctx, short)
+	require.Error(t, err)
+}
+
+func TestSetAddr(t *testing.T) {
+	store, err := New("")
+	require.NoError(t, err)
+
+	data := model.StoreData{
+		OriginalURL: "ya.ru",
+		ShortURL:    "short"}
+
+	ctx, close := context.WithCancel(context.Background())
+	err = store.Set(ctx, data)
+	require.NoError(t, err)
+
+	err = store.Set(ctx, data)
+	require.ErrorIs(t, err, storage.ErrAddressConflict)
+
+	err = store.Set(ctx, model.StoreData{})
+	require.Error(t, err)
+
+	close()
+	err = store.Set(ctx, data)
+	require.Error(t, err)
+
+}
 
 func TestUpdateAddr(t *testing.T) {
 
@@ -27,11 +106,9 @@ func TestUpdateAddr(t *testing.T) {
 	}
 
 	storage, err := New("")
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
-	ctx := context.Background()
+	ctx, close := context.WithCancel(context.Background())
 
 	for _, c := range cases {
 		data := []model.StoreData{
@@ -53,6 +130,57 @@ func TestUpdateAddr(t *testing.T) {
 			t.Errorf("want: %s, %s; get %#v ", c.short, c.addr, get)
 		}
 	}
+
+	close()
+	err = storage.Update(ctx, nil)
+	require.Error(t, err)
+}
+
+func TestGetUserURLs(t *testing.T) {
+	store, err := New("")
+	require.NoError(t, err)
+
+	data := model.StoreData{
+		UserID:      "user",
+		ShortURL:    "short",
+		OriginalURL: "ya.ru"}
+
+	ctx, close := context.WithCancel(context.Background())
+	err = store.Set(ctx, data)
+	require.NoError(t, err)
+
+	get, err := store.GetUserURLs(ctx, "user")
+	require.NoError(t, err)
+	require.Equal(t, 1, len(get))
+
+	get, err = store.GetUserURLs(ctx, "-")
+	require.NoError(t, err)
+	require.Equal(t, 0, len(get))
+
+	close()
+	_, err = store.GetUserURLs(ctx, "-")
+	require.Error(t, err)
+}
+
+func TestDeleteShort(t *testing.T) {
+	store, err := New("")
+	require.NoError(t, err)
+
+	data := model.StoreData{
+		UserID:      "user",
+		ShortURL:    "short",
+		OriginalURL: "ya.ru"}
+
+	ctx, close := context.WithCancel(context.Background())
+	err = store.Set(ctx, data)
+	require.NoError(t, err)
+
+	err = store.DeleteShort(ctx, []string{data.ShortURL})
+	require.NoError(t, err)
+
+	close()
+	err = store.DeleteShort(ctx, nil)
+	require.Error(t, err)
 }
 
 func BenchmarkGetAddr(b *testing.B) {
